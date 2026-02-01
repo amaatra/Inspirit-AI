03267cd013dbfbe6124ec14ffdb231b2a532b48a
amaatra <reachamaatra@gmail.com>
2026-02-01 17:45:02 +0530

    CNN
    Inspired by the VGG16 architecture, but less complex to accomodate for the small dataset

--- cnn.py
+++ cnn.py
@@ -9,9 +9,10 @@ import librosa
 
 import tensorflow as tf
 from tensorflow import keras
-from tensorflow.keras.preprocessing.sequence import pad_sequences # type: ignore
 from tensorflow.keras.models import Sequential # type: ignore
-from tensorflow.keras.layers import LSTM, Dense, Masking, Bidirectional, Dropout # type: ignore
+from tensorflow.keras.layers import Dense, Conv2D, InputLayer
+from tensorflow.keras.layers import Activation, MaxPooling2D, Dropout, Flatten, Reshape
+from tensorflow.keras.utils import to_categorical
 
 # 1 – LOAD AND PREPROCESS AUDIO FILES
 
@@ -23,7 +24,7 @@ maj_min_classn = []
 scaler = MinMaxScaler(feature_range=(0, 1))
 sr_target = 22050
 hop_length = 1024
-EPOCHS = 1
+EPOCHS = 70
 BATCHSIZE = 8
 
 def extract_chroma_features(filepath, sr_target, hop_length):
@@ -45,10 +46,9 @@ for filename in files_fullname:
     frames_per_file.append(len(chroma))
 
 print(len(chroma_frames))
-chroma_frames = [scaler.fit_transform(seq) for seq in chroma_frames] # seq = one element in the chroma_frames array = one "sequence" of chroma vectors...
 
 # chroma_frames_pad = pad_sequences(chroma_frames, padding="post", dtype="float32")
-# chroma_frames_pad is a 3-dimensional array with shape (288, 65, 12). 288 refers to the number of files which have been converted to chroma frames, 65 refers to
+# chroma_frames_pad is a 3-dimensional array with shape (288, 12, 65). 288 refers to the number of files which have been converted to chroma frames, 65 refers to
 # the number of frames per file and 12 refers to the number of dimensions in each frame
 
 # 2 – SPLITTING INTO TRAIN/VALIDATION/TEST
@@ -60,14 +60,55 @@ X_train, X_temp, y_train, y_temp = train_test_split(chroma_frames, maj_min_class
 
 X_val, X_test, y_val, y_test = train_test_split(X_temp, y_temp, test_size=0.5, random_state=42, stratify=y_temp) # Splitting temp into validation/test
 
+X_train_reshaped = np.vstack(X_train)
+scaler.fit(X_train_reshaped)
+
+X_train = np.array([scaler.transform(x) for x in X_train])
+X_val   = np.array([scaler.transform(x) for x in X_val])
+X_test  = np.array([scaler.transform(x) for x in X_test])
 
 # 3 – BUILDING THE CNN – NOT DONE YET
 
-model = Sequential([
-    # AHhh probably just going to replicate VGG16 architecture cause im not sure if I should reshape the data >.<
-])
+model = Sequential()
 
-model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
+model.add(InputLayer(input_shape=(12,65)))
+model.add(Reshape((12, 65, 1)))
+
+model.add(Conv2D(64, (3, 3), padding='same'))
+model.add(Activation('relu'))
+model.add(Conv2D(64, (3, 3), padding='same'))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=2))
+model.add(Dropout(0.1))
+
+model.add(Conv2D(128, (3, 3), padding='same'))
+model.add(Activation('relu'))
+model.add(Conv2D(128, (3, 3), padding='same'))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=2))
+model.add(Dropout(0.1))
+
+""" model.add(Conv2D(256, (3, 3), padding='same'))
+model.add(Activation('relu'))
+model.add(Conv2D(256, (3, 3), padding='same'))
+model.add(Activation('relu'))
+model.add(Conv2D(256, (3, 3), padding='same'))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=2))
+model.add(Dropout(0.5)) """
+
+# Not doing the full VGG16 model because the pooling may collapse information too aggressively...!
+
+model.add(Flatten())
+model.add(Dense(512))
+model.add(Activation('relu'))
+# model.add(Dropout(0.5))
+model.add(Dense(2))
+model.add(Activation('softmax'))
+
+opt = keras.optimizers.RMSprop(learning_rate=0.0001)
+
+model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
 
 model.summary()
 
@@ -75,19 +116,29 @@ model.summary()
 
 history = model.fit(X_train, y_train, validation_data=(X_val, y_val), epochs=EPOCHS, batch_size=BATCHSIZE)
 
+model.summary()
+
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+
+plt.legend(['train', 'validation', 'train_loss', 'val_loss'], loc='upper left')
+plt.show()
+
 # 5 – EVALUATIING THE MODEL
 
 # val_loss, val_acc = model.evaluate(X_val, y_val)
 # print("Validation accuracy:", val_acc)
 
 y_prob = model.predict(X_val)
-y_pred = (y_prob > 0.5).astype(int).flatten()
+y_pred = np.argmax(y_prob, axis=1)
 
 def show_confusion_matrix():
     cm = confusion_matrix(y_val, y_pred)
     disp = ConfusionMatrixDisplay(cm, display_labels=["Major", "Minor"])
     disp.plot(cmap=plt.cm.Blues)
-    plt.title("Confusion Matrix (LSTM)")
+    plt.title("Confusion Matrix (CNN)")
     plt.show()
 
 show_cm = input("Show confusion matrix? y/n")
